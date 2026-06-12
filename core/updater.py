72b2927949486420670ee5cee56b07e39054e772
FTPDev <destrooyy@protonmail.com>
2026-06-12 14:28:05 +0300

    fix: correct releases URL (FMailSender not EmailSenderPro)

--- core/updater.py
+++ core/updater.py
@@ -1,16 +1,18 @@
 """
-Автоматическое обновление отключено согласно правилам площадки (п. 1.8).
-Новые версии публикуются на странице релизов:
-https://github.com/FTPLabs/EmailSenderPro/releases
-"""
-
-def check_for_updates(*args, **kwargs):
-    return None
+  Автоматическое обновление отключено согласно правилам площадки (п. 1.8).
+  Новые версии публикуются на странице релизов:
+  https://github.com/FTPLabs/FMailSender/releases
+  """
 
 
-def apply_update_windows(*args, **kwargs):
-    return False
+  def check_for_updates(*args, **kwargs):
+      return None
 
 
-def is_newer(*args, **kwargs):
-    return False
+  def apply_update_windows(*args, **kwargs):
+      return False
+
+
+  def is_newer(*args, **kwargs):
+      return False
+  
